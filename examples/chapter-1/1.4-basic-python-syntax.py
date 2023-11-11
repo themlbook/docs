ca92e8a2c13e5aedda04d3e0e80ca35049950916
Chase Bellisime <Bellischase@gmail.com>
2023-11-11 09:18:57 -0800

    python-syntax updates

--- examples/chapter-1/1.4-basic-python-syntax.py
+++ examples/chapter-1/1.4-basic-python-syntax.py
@@ -1,6 +1,37 @@
+print("Hello, Python!")
+
 # Example of variable assignment
 message = "Hello, Python!"
 count = 42
 pi_value = 3.14
 
-message = -42
+message = -12 # message is now an int
+
+# Example of variable re-assignment
+message = "Hello, Python!"
+
+print(message) # prints the variable message to the console
+print("This is a string literal") # prints the string literal "This is a string literal" to the console
+print(5) # prints the integer 5 to the console
+print(pi_value) # prints the variable pi_value to the console
+
+sum_result = 5 + 3
+difference_result = 10 - 4
+product_result = 6 * 7
+quotient_result = 20 / 4
+expo_result = 2 ** 3
+
+# Example of operations on variables
+x = 5
+y = 3
+result = x + y
+print(result) # print the sum of x + y
+result = x * y # reassign result to the product of x * y
+print(result)
+
+# Example of shorthand operations
+x = 5
+x += 3 # equivalent to x = x + 3
+print(x)
+x -= 3 # equivalent to x = x - 3
+print(x)
